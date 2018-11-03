d1883483dac248d9a812c7faf01f616f8fdb6e64
TweetyTheBird <42115728+TweetyTheBird@users.noreply.github.com>
2018-11-03 10:35:02 +0200

    homework5 - fix operator in task 1 and formatting

--- homework5/public_html/js/homework5.js
+++ homework5/public_html/js/homework5.js
@@ -24,9 +24,9 @@ var animals = [
 ];
 
 var smallAnimals = [];
-smallAnimals = animals.filter(function(animal){
-    return (animal.size = 'small');
-}) ;
+smallAnimals = animals.filter(function (animal) {
+    return (animal.size == 'small');
+});
 
 console.log("(task 1) small animals:");
 console.log(smallAnimals);
@@ -36,8 +36,8 @@ console.log(smallAnimals);
 var arr_radii = [10, 15, 20];
 
 var areas = [];
-areas = arr_radii.map(function(radius){
-    var area = Math.PI*radius*radius;
+areas = arr_radii.map(function (radius) {
+    var area = Math.PI * radius * radius;
     return area;
 });
 
@@ -47,16 +47,16 @@ console.log(areas);
 //======================task three - average under 18==========================
 
 var persons = [
-    { name : 'Pesho', age: 19},
-    { name : 'Gosho', age: 16},
-    { name : 'Spaska', age: 12},
-    { name : 'Maria', age: 18},
-    { name : 'Ivan', age: 15},
-    { name : 'Ginka', age: 21}
+    {name: 'Pesho', age: 19},
+    {name: 'Gosho', age: 16},
+    {name: 'Spaska', age: 12},
+    {name: 'Maria', age: 18},
+    {name: 'Ivan', age: 15},
+    {name: 'Ginka', age: 21}
 ];
 
 
-var under18 = persons.filter(function(person){
+var under18 = persons.filter(function (person) {
     return person.age <= 18;
 });
 
@@ -65,7 +65,7 @@ for (var i = 0, count = 0, totAge = 0; i < under18.length; i++) {
     count++;
 }
 
-var avrgAge = totAge/count;
+var avrgAge = totAge / count;
 console.log("(task 3) Average age of people under 18:");
 console.log(avrgAge);
 
@@ -73,19 +73,19 @@ console.log(avrgAge);
 
 var inputWord = 'testWord';
 
-function searchForString(searchString,inputWord) {
+function searchForString(searchString, inputWord) {
 
-if (inputWord.search(searchString) != '-1') {
-    return true;
-} else {
-    return false;
-}
+    if (inputWord.search(searchString) != '-1') {
+        return true;
+    } else {
+        return false;
+    }
 
 }
 
 console.log("(task 4) search for a string:");
-console.log("should be true", searchForString('test',inputWord));
-console.log("should be false", searchForString('missing',inputWord));
+console.log("should be true", searchForString('test', inputWord));
+console.log("should be false", searchForString('missing', inputWord));
 
 
 
